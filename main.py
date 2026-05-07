a8b2fb1cf941e9420ae96df619fec81335c52b1f
awwann <annasumskaya@gmail.com>
2026-05-07 14:07:56 +0300

    Создала список категорий, обьявила JSON файл и создала главное окно Tkinter

--- main.py
+++ main.py
@@ -3,3 +3,14 @@ from tkinter import ttk, messagebox
 from tkcalendar import DateEntry
 import json
 from datetime import datetime
+
+# --- Конфигурация ---
+CATEGORIES = ["Еда", "Транспорт", "Развлечения", "Жилье", "Здоровье"]
+JSON_FILE = "data.json"
+
+class ExpenseTrackerApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Expense Tracker")
+        self.root.geometry("800x600")
+        self.root.resizable(False, False)
