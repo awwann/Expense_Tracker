640a064a26c912987683a3e2604a57fef8f056c3
awwann <annasumskaya@gmail.com>
2026-05-07 14:18:40 +0300

    Добавила загрузку данный из файла и фрейм ввода данных

--- main.py
+++ main.py
@@ -14,3 +14,11 @@ class ExpenseTrackerApp:
         self.root.title("Expense Tracker")
         self.root.geometry("800x600")
         self.root.resizable(False, False)
+
+        # Загрузка данных из файла
+        self.expenses = self.load_expenses()
+
+        # --- Виджеты ---
+        # Фрейм ввода данных
+        input_frame = tk.LabelFrame(root, text="Добавить расход", padx=10, pady=10)
+        input_frame.pack(pady=10, fill='x', padx=20)
