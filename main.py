4a3fdda08e50ccb5728c92b6d4bbb181df0c525d
awwann <annasumskaya@gmail.com>
2026-05-07 14:21:29 +0300

    Добавила сумму, категорию через Combobox и дату

--- main.py
+++ main.py
@@ -22,3 +22,19 @@ class ExpenseTrackerApp:
         # Фрейм ввода данных
         input_frame = tk.LabelFrame(root, text="Добавить расход", padx=10, pady=10)
         input_frame.pack(pady=10, fill='x', padx=20)
+
+        # Сумма
+        tk.Label(input_frame, text="Сумма:").grid(row=0, column=0, sticky='w')
+        self.sum_entry = tk.Entry(input_frame, width=15)
+        self.sum_entry.grid(row=0, column=1, padx=5)
+
+        # Категория
+        tk.Label(input_frame, text="Категория:").grid(row=0, column=2, sticky='w')
+        self.category_combobox = ttk.Combobox(input_frame, values=CATEGORIES, state="readonly", width=15)
+        self.category_combobox.current(0)
+        self.category_combobox.grid(row=0, column=3, padx=5)
+
+        # Дата
+        tk.Label(input_frame, text="Дата:").grid(row=0, column=4, sticky='w')
+        self.date_entry = DateEntry(input_frame, date_pattern='dd.mm.yyyy', width=15)
+        self.date_entry.grid(row=0, column=5, padx=5)
